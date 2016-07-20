7f1960db3b3d4c17ac805d1fd9a784aa7b4cb963
matrosov.d <madiv@list.ru>
2016-07-20 22:18:51 +0300

    start

--- CalcDohodaConsole/Program.cs
+++ CalcDohodaConsole/Program.cs
@@ -10,6 +10,18 @@ namespace CalcDohodaConsole
     {
         static void Main(string[] args)
         {
+            double stavka = 6.3;
+            double vklad = 100000;
+            double mes = 36;
+            double effectProcent = 0;
+
+            effectProcent = (1 + stavka / 1200);
+            effectProcent = Math.Pow(effectProcent,mes);
+            effectProcent = effectProcent - 1;
+            effectProcent = effectProcent * (12 / mes);
+            
+
+            
         }
     }
 }
