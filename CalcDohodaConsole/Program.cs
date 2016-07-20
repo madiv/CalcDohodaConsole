9ba2c89f1eaab07bead3e810797842d275bfdf94
matrosov.d <madiv@list.ru>
2016-07-20 22:35:48 +0300

    start

--- CalcDohodaConsole/Program.cs
+++ CalcDohodaConsole/Program.cs
@@ -12,13 +12,17 @@ namespace CalcDohodaConsole
         {
             double stavka = 6.3;
             double vklad = 100000;
-            double mes = 36;
+            double years = 3;
+            double mes = years * 12;
+            double dni = years * 365;
             double effectProcent = 0;
 
             effectProcent = (1 + stavka / 1200);
             effectProcent = Math.Pow(effectProcent,mes);
             effectProcent = effectProcent - 1;
             effectProcent = effectProcent * (12 / mes);
+
+            vklad = vklad + ((vklad * effectProcent) / 365) * dni;
             
 
             
